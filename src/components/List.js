e6bd140bbd925a8355f9a151d4e92bce2f8c52f3
JungwonSeong <yoeubi28@gmail.com>
2019-05-18 16:40:23 +0900

    add style

--- src/components/List.js
+++ src/components/List.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import style from './List.scss'
+import style from './List.scss';
 export default function List({ gif, idx }) {
   return (
     <li className={style.list}>
