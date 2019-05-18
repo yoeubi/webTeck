3e4fad01360b0de4e3cd93efaf9509f1c9e211c0
CressZZ <dktsjssnrnsi@gmail.com>
2019-05-18 14:45:28 +0900

    리스트 초기 세팅

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Search from './components/Search';
+import List from './components/List';
 
 const App = () => {
   return (
     <div>
       <Search />
+      <List />
+
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'mobx-react';
 import KeywordStore from './stores/keywordStore';
+import GifListStore from './stores/GifListStore';
 
 const keywordStore = new KeywordStore();
+const gifListStore = new GifListStore();
 
 ReactDOM.render(
-  <Provider keywordStore={keywordStore}>
+  <Provider keywordStore={keywordStore} gifListStore={gifListStore}>
     <App />
   </Provider>,
   document.querySelector('#root'),
