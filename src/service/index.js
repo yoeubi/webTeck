45d76b20465897da4717c70f0d80a3bea31a2f4a
JungwonSeong <yoeubi28@gmail.com>
2019-06-27 20:41:13 +0900

    add https

--- src/service/index.js
+++ src/service/index.js
@@ -2,26 +2,25 @@ import axios from 'axios';
 
 let isRequset = false;
 
-export const getList = (keyword, offset=0) => {
-    
-    if(isRequset) return false;
-    isRequset = true;
-    console.log(offset)
-    let res = axios({
-      method: 'GET',
-      url: 'http://api.giphy.com/v1/gifs/search',
-      params: {
-        api_key: '5jAu4p6gh8P4bAUJzhn99DWSKcKZS14r',
-        q: keyword,
-        offset: offset,
-        limit: 10,
-        
-      },
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    });
-    res.then(()=>{isRequset = false;})
-    return res;
-    
-  };
+export const getList = (keyword, offset = 0) => {
+  if (isRequset) return false;
+  isRequset = true;
+  console.log(offset);
+  let res = axios({
+    method: 'GET',
+    url: 'https://api.giphy.com/v1/gifs/search',
+    params: {
+      api_key: '5jAu4p6gh8P4bAUJzhn99DWSKcKZS14r',
+      q: keyword,
+      offset: offset,
+      limit: 10,
+    },
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  res.then(() => {
+    isRequset = false;
+  });
+  return res;
+};
